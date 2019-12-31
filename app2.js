15b52fbea27f30ea697d5459024f4f82e57e2e05
ngallant <nicolasgallant01210@gmail.com>
2019-12-31 14:38:37 +0100

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,67 +1,70 @@
-var request = require ('request');
-var express = require ('express');
-var bodyParser = require ('body-parser');
+var request = require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-var path= require ("path");
+var path = require("path");
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.post('/webhook',function(req,res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/webhook', function(req, res) {
   console.log('Received a post request');
-  if (!req.body) return res.sendStatus(400) 
-    res.setHeader('Content-Type','application/json');
+  if (!req.body) return res.sendStatus(400)
+  res.setHeader('Content-Type', 'application/json');
   console.log('Here is the request from DialogFlow');
   console.log(req.body);
-    var city = req.body.queryResult.parameters['geo-city'];
-    var w = getWeather(city);
-    let response = " ";
-    let responseObj = {
-      fullfilmentText : response
-      ,fullfilmentMessages : [{"text": {"text": [w]}}]
-      ,"source":""
-    }
+  var city = req.body.queryResult.parameters['geo-city'];
+  var w = getWeather(city);
+  
+  let response = '';
+    
+  let responseObj = {
+    fullfilmentText: response
+    , fullfilmentMessages: [{ "text": { "text": [w] } }]
+    , "source": ""
+  }
   console.log("Here is the response to dialogbox");
   console.log(responseObj);
   return res.json(responseObj);
 });
 
 var apiKey = 'c8db7d3c494184411e3721de2125950a';
-var result
+var result;
 
-function cb (err,response,body){
-  if (err){
+function cb(err, response, body) {
+  if (err) {
     console.log("error:", error);
   }
   var weather = JSON.parse(body)
-  if (weather.message == 'city not found'){
-    result = "Unable to get weather" + weather.message;
+  if (weather.message === 'city not found') {
+    result = 'Unable to get weather' + weather.message;
   } else {
-    result = "Right now its" + weather.main.temp + ' degrees with ' + weather.weather[0].description;
+    result = 'Right now its' + weather.main.temp + ' degrees with ' + weather.weather[0].description;
   }
- }
+}
 
- function getWeather(city){
-   result = undefined;
-   var result = 'http://api.openweathermap.org/data/2.5/weather?q=$(city)&units=imperial&appid=$(apiKey)';
-   console.log(url);
-   var req = request (url,cb);
-    while (result === undefined){
-      require ('deasync').runLoopOnce();     
-    } 
+function getWeather(city) {
+  result = undefined;
+  var result = 'http://api.openweathermap.org/data/2.5/weather?q=$(city)&units=imperial&appid=$(apiKey)';
+  console.log(url);
+  var req = request(url, cb);
+  while (result === undefined) {
+    require('deasync').runLoopOnce();
+  }
   return result;
 }
 
-
-app.get('/',function(req,res){
-   res.sendFile(path.join(__dirname,'index.html'));
-})
-
-io.on('connection',function(client){
+io.on('connection', function(client) {
   console.log('Socket connection established');
-  client.on('SendLocation',function(data){
+  client.on('SendLocation', function(data) {
     console.log('Location Received');
     console.log(data);
   });
